7cc37dc20ca0931daad5e3278531a0ec0417168b
MuhamadAdrian <madrian.septiana@gmail.com>
2020-12-05 17:41:18 +0700

    Add firebase to project

--- nuxt.config.js
+++ nuxt.config.js
@@ -46,7 +46,28 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+    '@nuxtjs/firebase',
   ],
+
+  firebase: {
+    config: {
+      apiKey: 'AIzaSyC-9N8H9VasX89il9t4Sd-NSM3ZB5_5PJQ',
+      authDomain: 'mutzcraft-nuxt.firebaseapp.com',
+      projectId: 'mutzcraft-nuxt',
+      storageBucket: 'mutzcraft-nuxt.appspot.com',
+      messagingSenderId: '944843167176',
+      appId: '1:944843167176:web:a1567c5e33eeb371569cfd',
+      measurementId: 'G-GCFV2NZ59N',
+    },
+    services: {
+      auth: {
+        initialize: {
+          onAuthStateChangedAction: 'auth/onAuthStateChanged',
+        },
+        ssr: true,
+      }, // Just as example. Can be any other service.
+    },
+  },
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
