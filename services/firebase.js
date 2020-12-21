a02abebdd704206174ae037f4461f2ad3db0bed6
MuhamadAdrian <59602236+MuhamadAdrian@users.noreply.github.com>
2020-12-21 17:25:30 +0700

    Update firebase.js

--- services/firebase.js
+++ services/firebase.js
@@ -4,16 +4,6 @@ import 'firebase/auth'
 import 'firebase/firestore'
 import 'firebase/storage'
 
-const config = {
-  apiKey: 'AIzaSyC-9N8H9VasX89il9t4Sd-NSM3ZB5_5PJQ',
-  authDomain: 'mutzcraft-nuxt.firebaseapp.com',
-  projectId: 'mutzcraft-nuxt',
-  storageBucket: 'mutzcraft-nuxt.appspot.com',
-  messagingSenderId: '944843167176',
-  appId: '1:944843167176:web:a1567c5e33eeb371569cfd',
-  measurementId: 'G-GCFV2NZ59N',
-}
-
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
 }
